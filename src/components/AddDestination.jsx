0e47885ee4c832b90c2f33ccaf4331aff8852aa2
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-12-10 15:41:39 -0600

    Add Destination Controller Component

--- src/components/AddDestination.jsx
+++ src/components/AddDestination.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { useState } from "react";
 
 function AddDestination() {
+  const [newCity, setNewCity] = useState("");
+  const [newCountry, setNewCountry] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //addDestination
+
+    setNewCity("");
+    setNewCountry("");
+  };
+
   return (
     <div className="p-4 border">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="row col-8 offset-2">
           <h4>Enter a new Destination</h4>
           <div className="col-5 p-1">
@@ -11,6 +23,8 @@ function AddDestination() {
               type="text"
               className="form-control"
               placeholder="Enter city..."
+              value={newCity}
+              onChange={(e) => setNewCity(e.target.value)}
             />
           </div>
           <div className="col-5 p-1">
@@ -18,6 +32,8 @@ function AddDestination() {
               type="text"
               className="form-control"
               placeholder="Enter country..."
+              value={newCountry}
+              onChange={(e) => setNewCountry(e.target.value)}
             />
           </div>
           <div className="col-2 p-1">
