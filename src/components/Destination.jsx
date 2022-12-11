f1d20fda21373508a9e7105ddc2f7731873ebf79
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-12-11 09:58:48 -0600

    Assignment 19 Solution - Edit Functionality

--- src/components/Destination.jsx
+++ src/components/Destination.jsx
@@ -1,8 +1,43 @@
 import React from "react";
-import { useDeleteDestinationMutation } from "../api/destinationApi";
+import {
+  useDeleteDestinationMutation,
+  useUpdateDestinationMutation,
+} from "../api/destinationApi";
+import { useState } from "react";
 
 function Destination({ destination }) {
   const [deleteDestination] = useDeleteDestinationMutation();
+  const [isUpdating, setIsUpdating] = useState(false);
+  const [newCity, setNewCity] = useState("");
+  const [newCountry, setNewCountry] = useState("");
+  const [updateDestination] = useUpdateDestinationMutation();
+
+  const handleUpdate = () => {
+    let city = "",
+      country = "";
+
+    if (newCity == "") {
+      city = destination.city;
+    } else {
+      city = newCity;
+    }
+    if (newCountry == "") {
+      country = destination.country;
+    } else {
+      country = newCountry;
+    }
+
+    updateDestination({
+      id: destination.id,
+      city: city,
+      country: country,
+      daysNeeded: destination.daysNeeded,
+    });
+
+    setNewCity("");
+    setNewCountry("");
+    setIsUpdating(!isUpdating);
+  };
 
   return (
     <div
@@ -12,13 +47,60 @@ function Destination({ destination }) {
         borderTop: "1px solid #333",
       }}
     >
-      <div className="col-3 offset-3">
-        {destination.city}, {destination.country}
+      <div className="col-4 row offset-2">
+        <div className="row">
+          <div className="col-6 p-1">
+            {isUpdating ? (
+              <input
+                type="test"
+                placeholder="city..."
+                className="form-control"
+                defaultValue={destination.city}
+                onChange={(e) => setNewCity(e.target.value)}
+              />
+            ) : (
+              <span>{destination.city}</span>
+            )}
+          </div>
+          <div className="col-6 p-1">
+            {isUpdating ? (
+              <input
+                type="test"
+                placeholder="country..."
+                className="form-control"
+                defaultValue={destination.country}
+                onChange={(e) => setNewCountry(e.target.value)}
+              />
+            ) : (
+              <span>{destination.country}</span>
+            )}
+          </div>
+        </div>
       </div>
       <div className="col-1 text-warning">{destination.daysNeeded} days</div>
-      <div className="col-2">
+      <div className="col-3">
         <button
-          className="btn form-control btn-danger"
+          className="btn btn-warning m-1"
+          onClick={() => {
+            setIsUpdating(!isUpdating);
+          }}
+        >
+          {isUpdating ? "Cancel" : "Edit"}
+        </button>
+        {isUpdating ? (
+          <button
+            className="btn btn-primary"
+            onClick={() => {
+              handleUpdate();
+            }}
+          >
+            Update
+          </button>
+        ) : (
+          ""
+        )}
+        <button
+          className="btn btn-danger m-1"
           onClick={() => deleteDestination({ id: destination.id })}
         >
           Delete
