58641fb069fa8490bb4bd6b6e33d7b0755e025a9
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-12-11 09:32:47 -0600

    Response from Query and Parameters

--- src/api/destinationApi.jsx
+++ src/api/destinationApi.jsx
@@ -8,7 +8,12 @@ export const destinationAPI = createApi({
     //QUERY -> GET
     //MUTATION -> POST/PUT/DELETE
     getAllDestination: builder.query({
-      query: () => "destination",
+      query: () => ({
+        url: "destination",
+        method: "GET",
+        params: {},
+      }),
+      transformResponse: (res) => res.sort((a, b) => b.id - a.id),
       providesTags: ["Destinations"],
     }),
     // getAllDestination: builder.query({
