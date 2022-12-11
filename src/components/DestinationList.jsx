c4a0798eb8b22021a7ab5f3301f77ca4c3e0670e
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-12-11 09:25:21 -0600

    Assignment 18  - Delete Destination

--- src/components/DestinationList.jsx
+++ src/components/DestinationList.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { useGetAllDestinationQuery } from "../api/destinationApi";
+import { useDeleteDestinationMutation } from "../api/destinationApi";
 
 function DestinationList() {
   const { data, isLoading, isSuccess, isError, error } =
     useGetAllDestinationQuery();
+  const [deleteDestination] = useDeleteDestinationMutation();
 
   let content;
   if (isLoading) {
@@ -11,14 +13,29 @@ function DestinationList() {
   } else if (isSuccess) {
     content = data.map((destination) => {
       return (
-        <article key={destination.id}>
-          <div className="text-center text-info p-2">
-            <div>
-              {destination.city}, {destination.country} -{" "}
-              {destination.daysNeeded} days
-            </div>
+        <div
+          className="row py-1"
+          key={destination.id}
+          style={{
+            borderBottom: "1px solid #333",
+            borderTop: "1px solid #333",
+          }}
+        >
+          <div className="col-3 offset-3">
+            {destination.city}, {destination.country}
           </div>
-        </article>
+          <div className="col-1 text-warning">
+            {destination.daysNeeded} days
+          </div>
+          <div className="col-2">
+            <button
+              className="btn form-control btn-danger"
+              onClick={() => deleteDestination({ id: destination.id })}
+            >
+              Delete
+            </button>
+          </div>
+        </div>
       );
     });
   } else if (isError) {
